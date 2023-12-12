d0c4415d26abfbc1b8750c5004fbfdd1823433b0
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2023-12-12 15:05:13 -0500

    Created planet-facts__label-container elements

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+
+// VARIABLES
+const planetFacts = document.querySelector(".planet-facts");
+const planetFactsLabel = document.querySelectorAll(".planet-facts__label");
+
+planetFacts.addEventListener("click", function (e) {
+  const target = e.target;
+  if (!target.classList.contains("planet-facts__label")) return;
+});
