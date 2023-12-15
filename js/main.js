16e1d850b2e807d9cfc1613bccef297d83a07f06
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2023-12-15 16:50:06 -0500

    Wrote logic for the structure label

--- js/main.js
+++ js/main.js
@@ -7,11 +7,19 @@ let currentPlanet = "mercury";
 let currentPlanetData = planets.find(
   (planet) => planet.name.toLowerCase() === currentPlanet
 );
+let currentLabelTextContent = "overview";
 const planetFacts = document.querySelector(".planet-facts");
 const planetFactsLabel = document.querySelectorAll(".planet-facts__label");
+const planetImage = document.querySelector(".planet-info__img");
+const planetDetails = document.querySelector(".planet-info__details");
+const sourceLink = document.querySelector(".source__link");
+const sourceWindow = document.querySelector(".source__window");
 
 // HELPER FUNCTIONS
 function selectLabel(target) {
+  // Do nothing if the label is already selected
+  if (target.classList.contains("label--selected")) return;
+
   // Make label color white
   target.classList.remove("label--not-selected");
   target.classList.add("label--selected");
@@ -42,5 +50,27 @@ planetFacts.addEventListener("click", function (e) {
     } else {
       deselectLabel(label);
     }
+  }
+
+  const targetTextContent = target.textContent.toLowerCase();
+
+  if (targetTextContent === currentLabelTextContent) {
+    return;
+  } else {
+    currentLabelTextContent = targetTextContent;
+  }
+
+  if (targetTextContent === "overview") {
+    planetImage.src = currentPlanetData.images.planet;
+    planetDetails.innerHTML = currentPlanetData.overview.content;
+    sourceLink.href = currentPlanetData.overview.source;
+    sourceWindow.href = currentPlanetData.overview.source;
+  }
+
+  if (targetTextContent === "structure") {
+    planetImage.src = currentPlanetData.images.internal;
+    planetDetails.innerHTML = currentPlanetData.structure.content;
+    sourceLink.href = currentPlanetData.structure.source;
+    sourceWindow.href = currentPlanetData.structure.source;
   }
 });
